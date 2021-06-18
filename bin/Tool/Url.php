f1d927eb2b0ba145ce291ffbeeff6928965975bc
Zamralik <vuk.kulvar+github@gmail.com>
2021-06-19 00:46:35 +0200

    Rework URL

--- bin/Tool/Url.php
+++ bin/Tool/Url.php
@@ -8,16 +8,17 @@ use \Exception;
 
 url-example://master-account:Abc123Def456g@www.exemple.com:12345678/directory/file.ext?property-name=value#content-title
 |-origin----------------------------------------------------------||-path------------||-appendix-----------------------|
-|-scheme---|//|-authority-----------------------------------------||-path------------|?|-query-----------|#|-fragment--|
-|-scheme---|//|-authentication-----------|@|-address--------------||-path------------|?|-query-----------|#|-fragment--|
-|-scheme---|//|-username---|:|-password--|@|-host--------|:|-port-||-path------------|?|-query-----------|#|-fragment--|
+|-scheme--|://|-authority-----------------------------------------||-path------------|?|-query-----------|#|-fragment--|
+|-scheme--|://|-authentication-----------|@|-address--------------||-path------------|?|-query-----------|#|-fragment--|
+|-scheme--|://|-username---|:|-password--|@|-host--------|:|-port-||-path------------|?|-query-----------|#|-fragment--|
 
 You can get any block named above, but can only set the atomic ones.
 The query setter wants a hashmap that will be serialized.
 You're responsible for encoding special characters of the host and path.
-Elements that are automatically encoded using rawurlencode() :
+Elements that are automatically encoded :
 • username
 • password
+• path
 • query property names & values
 • fragment
 
@@ -41,7 +42,10 @@ class Url
 	private $query = null;
 	private $fragment = null;
 
-	public function __construct($url = null)
+	/**
+	* @param string $url
+	*/
+	public function __construct(string $url = null)
 	{
 		if (isset($url))
 		{
@@ -49,6 +53,9 @@ class Url
 		}
 	}
 
+	/**
+	* @return string
+	*/
 	public function __toString()
 	{
 		return $this->serialize();
@@ -67,29 +74,7 @@ class Url
 		$path = $this->getPath();
 		$appendix = $this->getAppendix();
 
-		if ($path)
-		{
-			return $origin . $path . $appendix;
-		}
-
-		if (empty($origin))
-		{
-			return $appendix ?: '';
-		}
-
-		$host = $this->getHost();
-
-		if (empty($host))
-		{
-			return $appendix ?: '';
-		}
-
-		if (empty($appendix))
-		{
-			return $origin ?: '';
-		}
-
-		return '';
+		return $origin . $path . $appendix;
 	}
 
 	/**
@@ -97,22 +82,21 @@ class Url
 	*/
 	public function getOrigin()
 	{
-		$origin = '';
-
-		$scheme = $this->getScheme();
 		$authority = $this->getAuthority();
 
-		if ($scheme)
+		if (empty($authority))
 		{
-			$origin .= $scheme;
+			return null;
 		}
 
-		if ($authority)
+		$scheme = $this->getScheme();
+
+		if (empty($scheme))
 		{
-			$origin .= '//' . $authority;
+			return '//' . $authority;
 		}
 
-		return $origin ?: null;
+		return $scheme . '://' . $authority;
 	}
 
 	/**
@@ -120,22 +104,21 @@ class Url
 	*/
 	public function getAuthority()
 	{
-		$authority = '';
-
 		$address = $this->getAddress();
 
-		if ($address)
+		if (empty($address))
 		{
-			$authentication = $this->getAuthentication();
-			if ($authentication)
-			{
-				$authority .= $authentication . '@';
-			}
-
-			$authority .= $address;
+			return null;
 		}
 
-		return $authority ?: null;
+		$authentication = $this->getAuthentication();
+
+		if (empty($authentication))
+		{
+			return $address;
+		}
+
+		return $authentication . '@' . $address;
 	}
 
 	/**
@@ -143,23 +126,21 @@ class Url
 	*/
 	public function getAddress()
 	{
-		$address = '';
-
 		$host = $this->getHost();
 
-		if ($host)
+		if (empty($host))
 		{
-			$address .= $host;
-
-			$port = $this->getPort();
-
-			if ($port)
-			{
-				$address .= ':' . $port;
-			}
+			return null;
 		}
 
-		return $address ?: null;
+		$port = $this->getPort();
+
+		if (empty($port))
+		{
+			return $host;
+		}
+
+		return $host . ':' . $port;
 	}
 
 	/**
@@ -167,23 +148,21 @@ class Url
 	*/
 	public function getAuthentication()
 	{
-		$authentication = '';
-
 		$username = $this->getUsername();
 
-		if ($username)
+		if (empty($username))
 		{
-			$authentication .= $username;
-
-			$password = $this->getPassword();
-
-			if ($password)
-			{
-				$authentication .= ':' . $password;
-			}
+			return null;
 		}
 
-		return $authentication ?: null;
+		$password = $this->getPassword();
+
+		if (empty($password))
+		{
+			return $username;
+		}
+
+		return $username . ':' . $password;
 	}
 
 	/**
@@ -191,22 +170,27 @@ class Url
 	*/
 	public function getAppendix()
 	{
-		$appendix = '';
-
 		$query = $this->getQuery();
 		$fragment = $this->getFragment();
 
-		if ($query)
+		if (empty($query) && empty($fragment))
+		{
+			return null;
+		}
+
+		$appendix = '';
+
+		if (isset($query))
 		{
 			$appendix .= '?' . $query;
 		}
 
-		if ($fragment)
+		if (isset($fragment))
 		{
 			$appendix .= '#'. $fragment;
 		}
 
-		return $appendix ?: null;
+		return $appendix;
 	}
 
 	/**
@@ -297,15 +281,95 @@ class Url
 	*/
 	public function parse(string $url)
 	{
-		$parts = isset($url) ? parse_url($url) : [];
-		$this->scheme = empty($parts['scheme']) ? null : ($parts['scheme'] . ':');
-		$this->username = $parts['user'] ?? null;
-		$this->password = $parts['pass'] ?? null;
-		$this->host = $parts['host'] ?? null;
-		$this->port = $parts['port'] ?? null;
-		$this->path = $parts['path'] ?? '';
-		$this->query = $parts['query'] ?? null;
-		$this->fragment = $parts['fragment'] ?? null;
+		$parts = parse_url($url);
+
+		// Scheme
+
+		if (empty($parts['scheme']))
+		{
+			$this->scheme = null;
+		}
+		else
+		{
+			$this->setScheme($parts['scheme']);
+		}
+
+		// Username
+
+		if (empty($parts['user']))
+		{
+			$this->username = null;
+		}
+		else
+		{
+			$this->setUsername(rawurldecode($parts['user']));
+		}
+
+		// Password
+
+		if (empty($parts['pass']))
+		{
+			$this->password = null;
+		}
+		else
+		{
+			$this->setPassword(rawurldecode($parts['pass']));
+		}
+
+		// Host
+
+		if (empty($host))
+		{
+			$this->host = null;
+		}
+		else
+		{
+			$this->setHost($parts['host']);
+		}
+
+		// Port
+
+		if (empty($port))
+		{
+			$this->port = null;
+		}
+		else
+		{
+			$this->setPort($parts['port']);
+		}
+
+		// Path
+
+		if (empty($parts['path']))
+		{
+			$this->path = '/';
+		}
+		else
+		{
+			$this->setPath(rawurldecode($parts['path']));
+		}
+
+		// Query
+
+		if (empty($parts['query']))
+		{
+			$this->query = null;
+		}
+		else
+		{
+			$this->setQuery($parts['query']);
+		}
+
+		// Fragment
+
+		if (empty($parts['fragment']))
+		{
+			$this->fragment = null;
+		}
+		else
+		{
+			$this->setFragment(rawurldecode($parts['fragment']));
+		}
 	}
 
 	/**
@@ -313,9 +377,14 @@ class Url
 	*/
 	public function setScheme(string $scheme)
 	{
+		if (empty($scheme))
+		{
+			throw new Exception('Empty scheme');
+		}
+
 		$scheme = strtolower($scheme);
 
-		if (!preg_match('~^[a-z]+(-[a-z]+)*:$~', $scheme))
+		if (!preg_match('~^[a-z]+(-[a-z]+)*$~', $scheme))
 		{
 			throw new Exception('Invalid scheme');
 		}
@@ -328,6 +397,11 @@ class Url
 	*/
 	public function setUsername(string $username)
 	{
+		if (empty($username))
+		{
+			throw new Exception('Empty username');
+		}
+
 		$this->username = rawurlencode($username);
 	}
 
@@ -336,6 +410,11 @@ class Url
 	*/
 	public function setPassword(string $password)
 	{
+		if (empty($password))
+		{
+			throw new Exception('Empty password');
+		}
+
 		$this->password = rawurlencode($password);
 	}
 
@@ -344,6 +423,11 @@ class Url
 	*/
 	public function setHost(string $host)
 	{
+		if (empty($host))
+		{
+			throw new Exception('Empty host');
+		}
+
 		$host = strtolower($host);
 
 		if (preg_match('~^[0-9.]+$~', $host))
@@ -376,7 +460,7 @@ class Url
 	*/
 	public function setPort(int $port)
 	{
-		if (empty($port) || $port < 1)
+		if (empty($port) || $port < 1  || $port > 65535)
 		{
 			throw new Exception('Invalid port');
 		}
@@ -389,14 +473,30 @@ class Url
 	*/
 	public function setPath(string $path)
 	{
+		if (empty($path) || $path[0] !== '/')
+		{
+			throw new Exception('Invalid path');
+		}
+
+		$path = strtolower($path);
+		$path = rawurlencode($path);
+		$path = str_replace('%2F', '/', $path);
+
 		$this->path = $path;
 	}
 
 	/**
-	* @param string $query
+	* It is safer to use setParameters()
+	*
+	* @param string $query The value is not validated
 	*/
 	public function setQuery(string $query)
 	{
+		if (empty($query))
+		{
+			throw new Exception('Empty query');
+		}
+
 		$this->query = $query;
 	}
 
@@ -405,6 +505,11 @@ class Url
 	*/
 	public function setParameters(array $parameters)
 	{
+		if (empty($parameters))
+		{
+			throw new Exception('Empty parameters');
+		}
+
 		$this->query = http_build_query($parameters);
 	}
 
@@ -413,6 +518,11 @@ class Url
 	*/
 	public function setFragment(string $fragment)
 	{
+		if (empty($fragment))
+		{
+			throw new Exception('Empty fragment');
+		}
+
 		$this->fragment = rawurlencode($fragment);
 	}
 
@@ -447,10 +557,15 @@ class Url
 
 	public function removePath()
 	{
-		$this->path = '';
+		$this->port = '/';
 	}
 
 	public function removeQuery()
+	{
+		$this->query = null;
+	}
+
+	public function removeParameters()
 	{
 		$this->query = null;
 	}
